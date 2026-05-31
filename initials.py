cbf80713048eeeaf5fcb61b28cbea2ace4986853
k1ngfranc1s <2425403571@live.gctu.edu.gh>
2026-05-30 20:13:59 -0400

    Submit Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Prints out my initials (KKFNKB) in stylized large letters.
+# Assignment Number: 1
+#
+# Name: KELVIN KING FRANCIS NII KWEI BOYE>
+# STUDENT ID:  2425403571
+# Email: 2425403571@live.gctu.edu.gh
+# Grader: MR. AUGUSTUS BUCKMAN
+#
+# On my honor, KELVIN KING FRANCIS NII KWEI BOYE, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This function handles the formatted output of the stylized initials.
+    print()
+    print("...KKFNKB")
+    print()
+    print("...KK........KK........KK........KK........FFFFFFFFFF..........NNNN....NN..........KK......KK..........BBBBBBBBBB.......")
+    print("...KK.......KK.........KK.......KK.........FFFFFFFFFF..........NNNNN...NN..........KK.....KK...........BB........BB.....")
+    print("...KK......KK..........KK......KK..........FF..................NN.NN...NN..........KK....KK............BB........BB.....")
+    print("...KK.....KK...........KK.....KK...........FF..................NN..NN..NN..........KK...KK.............BBBBBBBBBBB......")
+    print("...KKKKKK..............KKKKKK..............FFFFFFFFF...........NN...NN.NN..........KKKKKK..............BBBBBBBBBBB......")
+    print("...KKKKKK..............KKKKKK..............FFFFFFFFF...........NN....NNNN..........KKKKKK..............BB........BB.....")
+    print("...KK.....KK...........KK.....KK...........FF..................NN....NNNN..........KK...KK.............BB........BB.....")
+    print("...KK......KK..........KK......KK..........FF..................NN.....NNN..........KK....KK............BB........BB.....")
+    print("...KK.......KK....**...KK.......KK....**...FF.............**...NN......NN.....**...KK.....KK......**...BBBBBBBBBB.....**")
+    print("...KK........KK...**...KK........KK...**...FF.............**...NN......NN.....**...KK......KK.....**...BBBBBBBBB......**")
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
